6cdc9c2c876c914d51c868cc3780a0e8ce414d81
Aric Chokey <achokey21@gmail.com>
2017-04-05 17:22:46 -0400

    rewrote parser

--- boca-police-incidents/incidentParser.py
+++ boca-police-incidents/incidentParser.py
@@ -10,23 +10,30 @@ headers = ['type', 'date', 'address'];
 def main():
     parse_rows();
     write_new_file();
+    print(*parsed_rows, sep='\n')
 
 
 parsed_rows = []
 
 def parse_rows():
     with open(raw_incident_file) as csvfile:
-        csvreader = csv.reader(csvfile, quotechar='|')
+        csvreader = csv.reader(csvfile)
         for row in csvreader:
             crime_type = row[0]
-            # Remove incident number
-            crime_date = re.sub('\d{10}', '', row[1]);
-            crime_date = crime_date.replace('"', '') + ', '
-            crime_year = row[2].split()[0];
-            crime_address = re.sub('^.+?\s', '', row[2]);
-            crime_address = crime_address.title().replace('"', '') + ', Boca Raton, Florida'
-            parsed_rows.append([crime_type, crime_date + crime_year, crime_address])
-
+            # Remove incident number with regex
+            if row[2] == '':
+                row[1] = re.sub('\d{10}', '', row[1]);
+                crime_date = row[1].split(',')[0];
+                crime_year = row[1].split()[2];
+                crime_address = re.sub('^.+?\d{4}', '', row[1]);
+                crime_address = crime_address.title().strip() + ', Boca Raton, Florida'
+            else:
+                row[1] = re.sub('\d{10}', '', row[1]);
+                crime_date = row[1].split(',')[0];
+                crime_year = row[1].split()[2];
+                crime_address = row[2];
+                crime_address = crime_address.title().strip() + ', Boca Raton, Florida'                
+            parsed_rows.append([crime_type, crime_date + ', ' + crime_year, crime_address])
 
 def write_new_file():
     with open('boca-incidents-final.csv', 'w', newline='') as finalfile:
@@ -36,7 +43,6 @@ def write_new_file():
     
 
 if __name__ == "__main__":
-    # This function executes when you do "filingScraper.py" on the command line.
     print("Cleaning file now...");
     main();
     print("All done!");
