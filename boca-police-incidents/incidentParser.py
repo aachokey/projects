bb8445842ee180ca707c655ab18e7e8ab23456fc
Aric Chokey <achokey21@gmail.com>
2017-04-05 01:23:32 -0400

    add headers to parser, make it bougee

--- boca-police-incidents/incidentParser.py
+++ boca-police-incidents/incidentParser.py
@@ -1,21 +1,22 @@
 #! python3
-# incidentParser.py - parses incident data from the Boca Police Department.
+# incidentParser.py - parses incident data from the Boca Police Department and cleans it up.
 
 import re
 import csv
 
-incident_file = "bocaIncidents.csv"
-
+raw_incident_file = "bocaIncidents.csv"
+headers = ['type', 'date', 'address'];
 
 def main():
-    parse_rows()
-    # save_new_file()
+    parse_rows();
+    write_new_file();
 
+
+parsed_rows = []
 
 def parse_rows():
-    with open(incident_file) as csvfile:
+    with open(raw_incident_file) as csvfile:
         csvreader = csv.reader(csvfile, quotechar='|')
-        parsed_rows = []
         for row in csvreader:
             crime_type = row[0]
             # Remove incident number
@@ -25,13 +26,13 @@ def parse_rows():
             crime_address = re.sub('^.+?\s', '', row[2]);
             crime_address = crime_address.title().replace('"', '') + ', Boca Raton, Florida'
             parsed_rows.append([crime_type, crime_date + crime_year, crime_address])
-        write_new_file(parsed_rows)
 
 
-def write_new_file(iterable):
+def write_new_file():
     with open('boca-incidents-final.csv', 'w', newline='') as finalfile:
             writer = csv.writer(finalfile)
-            writer.writerows(iterable)
+            writer.writerow(headers)
+            writer.writerows(parsed_rows)
     
 
 if __name__ == "__main__":
