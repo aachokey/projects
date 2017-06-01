f58cbe85389c017c5ccf6ee18caf2c45cdc05621
Aric Chokey <achokey21@gmail.com>
2017-06-01 17:11:02 -0400

    scraper urls

--- boca-building-permits/permitScraper.py
+++ boca-building-permits/permitScraper.py
@@ -3,30 +3,35 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-url = input("Enter a url: ")
-# Applied permits: http://www.myboca.us/0122/Applied-Permits
-# Issued permits: http://www.myboca.us/1123/Issued-Permits
+# url = input("Enter a url: ")
+# Applied permits: http://apps.ci.boca-raton.fl.us/bp/applied-permits.php
+# Issued permits: http://apps.ci.boca-raton.fl.us/bp/issued-permits.php
 
+url = "http://apps.ci.boca-raton.fl.us/bp/issued-permits.php"
+
+link_list = []
 
 def main():
-	get_files();
+    get_files();
 
 
 def get_files():
-	r = requests.get(url)
-	if r.status_code == 200:
-		pass
-	else:
-		print("That URL doesn't work.")
-	print("Scraping permits...");
-	soup = BeautifulSoup(r.content, "html5lib");
-	print(r.content)
+    r = requests.get(url)
+    if int(r.status_code) == 200:
 
+        print("Scraping page...")
+        soup = BeautifulSoup(r.content, "html5lib");
+        page_links = soup.select('a[href]');
 
+        for link in page_links:
+            if 'xls' in link.text.strip().lower():
 
+    else:
+        print("That URL doesn't work.")
 
+        
 
 
 if __name__ == "__main__":
-	main();
-	print("All done!");
+    main();
+    print("All done!");
